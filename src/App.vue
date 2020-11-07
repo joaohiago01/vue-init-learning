80678b01c1578600dd658d4ea45cfae2e4e861bc
joaohiago01 <joaohiago96@gmail.com>
2020-11-07 17:55:23 -0300

    Init learning with Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Client />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Client from './components/Client.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Client
   }
 }
 </script>
---
+++ src/components/Client.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <strong>Vue.js</strong>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
